24fbfeda74e0b03c0c944f7f8c39c0da12d2f04c
Pavel <rut100500@yandex.ru>
2022-12-01 23:29:24 +0300

    Найдите сумму элементов  главной диагонали двумерного рандомного массива

---
+++ task51/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов находящихся на главной диагонали.
+
+Console.WriteLine("Введите количество строк - m");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.WriteLine("Введите количество столбцов - n");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if(!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибочный ввод! Не цифра! ПереВВедите!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+int sumdig = SumDigArray(array);
+Console.WriteLine($"Сумма элементов главной диагонали = {sumdig} ");
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+            
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+int SumDigArray (int [,] array) // Если возвращаться будет не массив, а число, то НЕ int[,]? а просто int
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum += array[i, j];
+            }
+        }
+        
+    }
+    return sum;
+}
