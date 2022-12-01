95196f8e3a79815a37651241b5d705705fd31c63
Pavel <rut100500@yandex.ru>
2022-12-01 23:27:57 +0300

    Нечетные индексы ранд массива заменить на их квадрат

---
+++ task49/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Найдите элементы у которых оба индекса нечетные и замените эти элементы на их квадраты
+
+Console.WriteLine("Введите количество строк - m");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.WriteLine("Введите количество столбцов - n");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if(!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибочный ввод! Не цифра! ПереВВедите!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+Console.WriteLine();
+ExchangeQuardeArray(array);
+Print2DArray(array);
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ExchangeQuardeArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i % 2 !=0 && j % 2 != 0) array[i, j] = array[i, j] * array[i, j];
+        }
+    }
+    return array;
+}
